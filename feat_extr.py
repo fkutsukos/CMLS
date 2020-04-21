c1fd5a3b0097dd30419bd0294d38bf329ab26376
Fotis Kutsukos <fotis.kutsukos@gmail.com>
2020-04-21 11:15:08 +0200

    Feature Selection tested with f_classif scoring function

--- feat_extr.py
+++ feat_extr.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 import os
+from pathlib import Path
+from sklearn.datasets import make_classification
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
 
 # %% DEFINITION OF THE FEATURES
@@ -29,7 +33,7 @@ def compute_specskew(spec):
     spread = np.sqrt(sum((k_axis - centr) ** 2 * abs(spec)) / sum(abs(spec)))
 
     m_th = sum((k_axis - centr) ** 3 * abs(spec)) / sum(abs(spec))
-    skew = m_th / (spread) ** 3
+    skew = m_th / spread ** 3
     return skew
 
 
@@ -43,14 +47,14 @@ def compute_speckurt(spec):
     return kurt
 
 
-def compute_rolloff(spec):
+def compute_rolloff(spec, fs):
     ROE = 0.95 * (sum(np.abs(spec) ** 2))
     E = 0
     for k in np.arange(len(spec)):
         E = E + np.abs(spec[k]) ** 2
         if E >= ROE:
             break
-    return k * (Fs / 2) / len(spec)
+    return k * (fs / 2) / len(spec)
 
 
 def compute_slope(spec):
@@ -131,17 +135,21 @@ def compute_zcr(win, Fs):
 4-string Bass	41Hz-392Hz	~4kHz-5kHz
 5-string Bass	31Hz-392Hz	~4kHz-5kHz
 6-string Bass	31Hz-523Hz	~4kHz-5kHz
+
+The notes cover the common pitch range of a 4-string bass guitar from E1 (41.2 Hz) to G3 (196.0 Hz) or the common 
+pitch range of a 6-string electric guitar from E2 (82.4 Hz) to E5 (659.3 Hz). 
 '''
 
 
 def pitch_detection(win, Fmin, Fmax, Fs):
-    spec_length = spec.shape[0]
+    # spec_length = spec.shape[0]
     xcorr = np.correlate(win, win, mode='full')
 
     # keep only the positive lags of autocorrelation
     rpos = xcorr[int(xcorr.size / 2):]
 
     # find maximum peak within the accepted range
+
     lagmin = int(np.floor(Fs / Fmax))
     lagmax = int(np.ceil(Fs / Fmin))
     rc = rpos[lagmin:lagmax]
@@ -163,9 +171,10 @@ def compute_autocorr(win):
     return result[int(result.size / 2):]
 
 
+'''
 # %%
 # file path
-file_path = 'data/Bass monophon/Samples/NoFX/B11-42304-1111-00031.wav'
+file_path = 'data/Bass monophon/Samples/EQ/B21-50312-1122-12675.wav'
 
 # %%
 # load and plot the audio file
@@ -176,7 +185,7 @@ plt.plot(time_axis, x)
 plt.xlim([time_axis[0], time_axis[-1]])
 plt.xlabel('Time (seconds)')
 plt.ylabel('Waveform')
-
+'''
 # %%
 # windowing
 '''
@@ -209,60 +218,65 @@ for i in np.arange(win_number):
 
     #insert the computation of the feature
 '''
-# Hanning window shaping factor L = 4 , bass minimum frequency 40 Hz.
-win_length = int(np.ceil((4 * Fs) / 40))
 
-window = sp.signal.get_window(window='hanning', Nx=win_length)
-
-# JND for a sound at 40 Hz is equal to 3Hz
-JND = 3
-# Peak localization
-fft_length = int(np.ceil(Fs / (2 * JND)))
-
-# FFT performance requires  N_FFT as a power of 2
-fft_length = int(2 ** (np.ceil(np.log2(fft_length))))
-
-# Hop size equal to 25% of the window length
-hop_size = int(np.floor(win_length / 8))
-
-# Number of frames
-frames_number = int(np.floor(x.shape[0] - win_length) / hop_size)
 
 # Training Features
-features = ['Spectral Centroid', 'Spectral Spread', 'Spectral Skewness', 'Spectral Kurtosis',
-            'Spectral Rolloff', 'Spectral Slope', 'Spectral Flatness', 'Spectral Flux', 'Zero Crossing Rate',
-            'Auto-correlation', 'Pitch Detection']
-train_features = np.zeros((len(features), frames_number))
-for i in np.arange(frames_number - 1):
 
-    frame = x[i * hop_size: i * hop_size + win_length]
-    frame_wind = frame * window
+def compute_features_frames(audio_file, Fs, features):
+    # Hanning window shaping factor L = 4 , bass minimum frequency 40 Hz.
+    win_length = int(np.ceil((4 * Fs) / 40))
+    window = sp.signal.get_window(window='hanning', Nx=win_length)
 
-    # compute spectrogram for every frame using zero-padding
-    spec = np.fft.fft(frame_wind, n=fft_length)
-    nyquist = int(np.floor(spec.shape[0] / 2))
-    spec = spec[1: nyquist]
+    # JND for a sound at 40 Hz is equal to 3Hz
+    JND = 3
+    # Peak localization
+    fft_length = int(np.ceil(Fs / (2 * JND)))
 
-    # compute spec features
-    if (sum(abs(spec)) != 0):
-        train_features[0, i] = compute_speccentr(spec)
-        train_features[1, i] = compute_specspread(spec)
-        train_features[2, i] = compute_specskew(spec)
-        train_features[3, i] = compute_speckurt(spec)
-        train_features[4, i] = compute_rolloff(spec)
-        train_features[5, i] = compute_slope(spec)
-        train_features[6, i] = compute_flatness(spec)
-        train_features[7, i] = compute_flux(frame_wind)
+    # FFT performance requires  N_FFT as a power of 2
+    fft_length = int(2 ** (np.ceil(np.log2(fft_length))))
+
+    # Hop size equal to 25% of the window length
+    hop_size = int(np.floor(win_length / 8))
+
+    # Number of frames
+    frames_number = int(np.floor(audio_file.shape[0] - win_length) / hop_size)
+
+    train_features_frames = np.zeros((len(features), frames_number))
+
+    for i in np.arange(frames_number - 1):
+
+        frame = audio_file[i * hop_size: i * hop_size + win_length]
+        frame_wind = frame * window
+
+        # compute spectrogram for every frame using zero-padding
+        spec = np.fft.fft(frame_wind, n=fft_length)
+        nyquist = int(np.floor(spec.shape[0] / 2))
+        spec = spec[1: nyquist]
+
+        # compute spec features
+        if (sum(abs(spec)) != 0):
+            train_features_frames[0, i] = compute_speccentr(spec)
+            train_features_frames[1, i] = compute_specspread(spec)
+            train_features_frames[2, i] = compute_specskew(spec)
+            train_features_frames[3, i] = compute_speckurt(spec)
+            train_features_frames[4, i] = compute_rolloff(spec, Fs)
+            train_features_frames[5, i] = compute_slope(spec)
+            train_features_frames[6, i] = compute_flatness(spec)
+            train_features_frames[7, i] = compute_flux(frame_wind)
 
         # compute time features
-        train_features[8, i] = compute_zcr(frame_wind, Fs)
-        # train_features[9, i] = compute_autocorr(frame_wind)
+        train_features_frames[8, i] = compute_zcr(frame_wind, Fs)
+        # train_features_frames[9, i] = compute_autocorr(frame_wind)
 
-    # compute harmonic features
-    train_features[10, i] = pitch_detection(frame_wind, 40, 400, Fs)
+        # compute harmonic features
+        # train_features_frames[9, i] = librosa.effects.harmonic(audio_file)
 
-    # compute mfcc features
-    #    train_spec_feature[8, i] = compute_mfcc_librosa()
+
+        # compute cepstrum features
+        # train_features_frames[11, i] = compute_mfcc_librosa()
+
+    return train_features_frames
+
 
 '''
 # Plotting Computed Features
@@ -276,30 +290,49 @@ for index, feature in enumerate(features):
 
 classes = ['Distortion', 'NoFX', 'Tremolo']
 
-dict_train_features = {'Distortion': [], 'NoFX': [], 'Tremolo': []}
-'''
+dict_train_features = {'Distortion': []}  # , 'NoFX': [], 'Tremolo': []}
+
+num_audio_files = 10
+features = ['Spectral Centroid', 'Spectral Spread', 'Spectral Skewness', 'Spectral Kurtosis',
+            'Spectral Rolloff', 'Spectral Slope', 'Spectral Flatness', 'Spectral Flux', 'Zero Crossing Rate',
+            'Pitch Detection']
+X = np.zeros((num_audio_files, len(features)))
+y = np.zeros(3*num_audio_files)
+count = 0
 for c in classes:
-    train_root = 'data/{}/training/'.format(c)
+    train_root = 'data/training/{}'.format(c)
     class_train_files = [f for f in os.listdir(train_root) if f.endswith('.wav')]
-    n_train_samples = len(class_train_files)
+    n_train_audio_files = len(class_train_files)
 
-    train_features = np.zeros((n_train_samples, n_mfcc))
+    train_features_mean = np.zeros((n_train_audio_files, len(features)))
+
     for index, f in enumerate(class_train_files):
+        if index == num_audio_files:
+            break
         audio, fs = librosa.load(os.path.join(train_root, f), sr=None)
-        mfcc = compute_mfcc(audio, fs, n_mfcc)
-        train_features[index, :] = np.mean(mfcc, axis=1)
+        audio_train_features_frames = compute_features_frames(audio, fs, features)
+        train_features_mean[index, :] = np.mean(audio_train_features_frames, axis=1)
 
-    dict_train_features[c] = train_features
-'''
+    if count == 0:
+        X = train_features_mean[0:num_audio_files, :]
 
+    else:
+        X = np.concatenate((X, train_features_mean[0:num_audio_files, :]))
 
-'''
-time_axis = np.arange(x.shape[0]) / Fs
-plt.figure(figsize=(16, 4))
-plt.plot(time_axis, x)
-plt.xlim([time_axis[0], time_axis[-1]])
-plt.xlabel('Time (seconds)')
-plt.ylabel('Waveform')
+    if c == "Distortion":
+        y[0:num_audio_files] = 0
+    if c == "NoFX":
+        y[num_audio_files:2*num_audio_files] = 1
+    if c == "Tremolo":
+        y[2*num_audio_files: 3*num_audio_files] = 2
+    count = count + 1
+
+# define feature selection
+selectedFeatures = SelectKBest(score_func=f_classif, k=3)
+# apply feature selection
+X_selected = selectedFeatures.fit_transform(X, y)
+# print(X_selected.shape)
+
 '''
 harmonic_features = librosa.effects.harmonic(x)
 spec = np.abs(np.fft.fft(harmonic_features))
@@ -307,15 +340,20 @@ nyquist = int(np.floor(spec.shape[0] / 2))
 
 spec = spec[1:nyquist]
 
-frequency_axis = np.arange(spec.shape[0])*(Fs/(2*nyquist))
+fundamental_index = np.argmax(spec)
+fundamental_freq = fundamental_index * (Fs / 2) / spec.shape[0]
+
+frequency_axis = np.arange(spec.shape[0]) * (Fs / 2) / spec.shape[0]
 
 plt.figure(figsize=(10, 2))
 plt.xlim([frequency_axis[0], frequency_axis[1000]])
 plt.title('Harmonic Features')
 plt.xlabel('frequencies')
 plt.ylabel('Amplitude')
-plt.plot(frequency_axis,spec)
+plt.plot(frequency_axis, spec)
 plt.show()
+'''
+
 '''
 win_length = int(np.floor(0.01 * Fs))
 hop_size = int(np.floor(0.0075 * Fs))
